11503341dc540d080ccb76303d8af35394e57e63
Max Shcherbinin <maksimserbinin9119@gmail.com>
2020-05-31 02:58:34 +0300

    add blade and vue admin

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,10 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
+Vue.component('table-admin',require('./components/admin/TableAdmin').default);
+Vue.component('create-user',require('./components/admin/user/CreateUser').default);
+Vue.component('edit-user',require('./components/admin/user/EditUser').default);
+Vue.component('index-user',require('./components/admin/user/IndexUser').default);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
